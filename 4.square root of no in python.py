ab8f6352530fd3b411e93de5c54ba55c6984bb23
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-13 21:53:27 +0530

    4.square root of no in python.py

---
+++ 4.square root of no in python.py
@@ -0,0 +1,15 @@
+//Using the math.sqrt() function:
+import math
+number = 25
+sqrt = math.sqrt(number)
+print(f"The square root of {number} is {sqrt}")
+
+#The square root of 25 is 5.0
+
+
+//Using the exponentiation operator ``:**
+number = 16
+sqrt = number ** 0.5
+print("The square root of the given number is:" ,sqrt)
+
+#The square root of the given number is: 4.0
