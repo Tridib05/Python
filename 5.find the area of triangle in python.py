c1f53db0a9472f44d93bb1849febe4ac0d2d8b56
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-13 22:01:01 +0530

    Update 5.find the area of triangle in python.py

--- 5.find the area of triangle in python.py
+++ 5.find the area of triangle in python.py
@@ -10,4 +10,5 @@ s = (a + b + c) / 2
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
 print("The area of the triangle is :" ,area)
 
-//The area of the triangle is : 14.696938456699069
+#output:-
+The area of the triangle is : 14.696938456699069
