5f2e34b84eafb60143c6b157db33d27dd8b46a70
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-13 21:27:47 +0530

    print function in python.py

---
+++ print function in python.py
@@ -0,0 +1,25 @@
+a="123"
+b="32"
+print(a+b)              # 2024
+
+a=123
+b=32
+print(a+b)              # 155
+
+a=10
+b=10
+print(a,a)             # 10  10
+
+a=20
+b=20
+print(a,a,sep="*")     # 20*20
+
+e=32
+f=43
+print(e)               # 32
+print(f)               # 43
+
+c=45
+d=67
+print(c,end=" ")
+print(d)               # 45  67
