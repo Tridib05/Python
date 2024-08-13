c04fc459ce23bb2936ab42bf3d2afbee1b854546
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-13 22:29:28 +0530

    Create 11.largest among three numbers in python.py

---
+++ 11.largest among three numbers in python.py
@@ -0,0 +1,14 @@
+a=10
+b=14
+c=12
+if(a>=b)and(a>=c):
+    print("The largest number is :",a)
+elif(b>=a)and(b>=c):
+    print("The largest number is:",b)
+else:
+    print("The largest nmumber is:",c)
+
+    
+
+#output:-
+The largest number is: 14
