7fe18df04aea3e74ce01d19527511a701fb1b5ef
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-13 22:00:42 +0530

    Create 6.swap to variables in python.py

---
+++ 6.swap to variables in python.py
@@ -0,0 +1,16 @@
+# Python program to swap two variables
+
+x = 5
+y = 10
+
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print("The value of x after swapping:", x)
+print("The value of y after swapping:", y)
+
+#output:-
+The value of x after swapping: 10
+The value of y after swapping: 5
