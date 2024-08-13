6f4dd4658c16ff0dd84e0a792240bf30c2d8f3d4
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-13 22:10:08 +0530

    7.area of rectangle in python.py

---
+++ 7.area of rectangle in python.py
@@ -0,0 +1,12 @@
+# default input to the variables
+width = 4
+height = 5
+
+#  using the formula to calculate the Area
+Area = width * height
+
+# after calculating, simple print it in string
+print("Area of rectangle = ",Area)  
+
+#output:-
+Area of rectangle =  20
