253372931668c69aaa78585a96da8eafd7462e55
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-13 21:38:13 +0530

    2.input function in python.py

---
+++ 2.input function in python.py
@@ -0,0 +1,57 @@
+//arithmrtic operator
+a=int(input("Enter num1:"))
+b=int(input("Enter num2:"))
+c=a+b
+print(c)                     Enter num1:15                       
+e=a-b                        Enter num2:5
+print(e)                     15
+f=a*b                        5
+print(f)                     50
+g=a/b                        2.0
+print(g)                     0
+h=a%b                        100000
+print(h)                     2
+i=a**b
+print(i)
+j=a//b
+print(j)
+
+
+//comparision operator
+a=float(input("Enter num1:"))
+b=float(input("Enter num2:"))
+c=a>b
+print(c)                     Enter num1: 5.5  
+e=a<b                        Enter num2: 4.2
+print(e)                     True
+f=a==b                       False
+print(f)                     False
+g=a!=b                       True
+print(g)                     True
+h=a>=b                       False
+print(h)
+i=a<=b
+print(i)
+
+
+//assignment operator
+a,b=10,5
+a+=b
+print(a)                    # 15
+b,c=20,10
+b-=c
+print(b)                    # 10
+c,d=5,2
+c*=d
+print(c)                    # 10
+d,e=12,4
+d/=e
+print(d)                    # 3.0
+e,f=8,7
+e%=f
+print(e)                    # 1
+f,g=5,2
+f**=g
+print(f)                    # 25
+g,h=5,2
+print(g)                    # 5
