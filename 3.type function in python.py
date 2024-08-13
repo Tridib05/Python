e0388b5e9beda5441730131cbb34340f0b66a571
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-13 21:46:07 +0530

    3.type function in python.py

---
+++ 3.type function in python.py
@@ -0,0 +1,17 @@
+a=10
+print(type(a))                  <class 'int'>
+b=input(" ")                     10 
+print(type(b))                  <class 'str'> 
+c=int(b)
+print(type(c))                  <class 'int'> 
+
+
+
+d = "Hello World"          
+e = 10.23
+f = 11
+g='python'
+print(type(d))                  <class 'str'>
+print(type(e))                  <class 'float'>
+print(type(f))                  <class 'int'>
+print(type(g))                  <class 'str'>
