49e60a97d2235ea05fa766ba5fe54718825a647d
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-14 21:53:40 +0530

    Create 13.print 0-10 odd no in python.py

---
+++ 13.print 0-10 odd no in python.py
@@ -0,0 +1,27 @@
+//using max()
+maximum = 10
+
+for number in range(1, maximum + 1):
+    
+    if number % 2 != 0:
+        print(number)
+
+#Output:-
+1
+3
+5
+7
+9
+
+
+//using for loop
+for i in range(1, 10, 2):
+    print(i)
+
+#Output:-
+1
+3
+5
+7
+9
+  
