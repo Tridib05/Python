7f3b8a0c36107814a91c79f71c6034287bdde46c
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-14 21:51:03 +0530

    Create 12.print 0-10 even no in python.py

---
+++ 12.print 0-10 even no in python.py
@@ -0,0 +1,27 @@
+//using max()
+maximum = 10
+
+for number in range(0, maximum + 1):
+    
+    if number % 2 == 0:
+        print(number)
+
+#Output:-
+0
+2
+4
+6
+8
+10
+
+//using for loop
+for i in range(0, 11, 2):
+  print(i)
+
+#Output:-
+0
+2
+4
+6
+8
+10
