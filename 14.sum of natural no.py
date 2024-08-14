3a81cbb0c36d14479b1d228a6a7bcee5dda4c4e9
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-14 22:40:20 +0530

    Create 14.sum of natural no.py

---
+++ 14.sum of natural no.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number:"))
+sum = 0
+for i in range(1,num+1):
+  sum+=i
+print("The sum of first",num,"natural numbers is:",sum)
+
+#Output:-
+Enter a number:8
+The sum of first 8 natural numbers is: 36
