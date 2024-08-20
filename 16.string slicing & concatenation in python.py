d5573ea029a23e8dfd222f09ac58b5fbb420da82
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-20 22:08:03 +0530

    Create 16.string slicing & concatenation in python.py

---
+++ 16.string slicing & concatenation in python.py
@@ -0,0 +1,24 @@
+//string slicing
+S1="Learning python"
+print(S1[5:11:2])
+print(S1[::])
+print(S1[::-1])
+print(S1[-1:4:-1])
+print(S1[-1: :-2])
+
+#Output:-
+igp
+Learning python
+nohtyp gninraeL
+nohtyp gni
+nhy nnaL
+
+
+//string concatenate
+S1 = "Hello"
+S2 = "Global"
+C=S1+S2
+print("The concatenation of two strings is:",C)
+
+#Output:-
+The concatenation of two strings is: HelloGlobal
