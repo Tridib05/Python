a2c50fb2e1d3dd3d4c9ddad339be738809c70afb
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-22 21:54:23 +0530

    Update 5.find the area of triangle in python.py

--- 5.find the area of triangle in python.py
+++ 5.find the area of triangle in python.py
@@ -1,14 +1,14 @@
-# Python Program to find the area of triangle
+#Python Program to find the area of triangle
 a = 5
 b = 6
 c = 7
 
-# calculate the semi-perimeter
+#Calculate the semi-perimeter
 s = (a + b + c) / 2
 
-# calculate the area
+#Calculate the area
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
 print("The area of the triangle is :" ,area)
 
-#output:-
+#Output:-
 The area of the triangle is : 14.696938456699069
