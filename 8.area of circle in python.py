6799714c0b697118a57b5ce4b601d546ae1f0c54
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-08-22 21:55:22 +0530

    Update 8.area of circle in python.py

--- 8.area of circle in python.py
+++ 8.area of circle in python.py
@@ -1,12 +1,12 @@
-# default input to the variables
+#default input to the variables
 r=5
 
 #claculate trhe area of a circle formula
 area=3.14*(r*r)
 
-## Display the result, including the radius and calculated area
+#Display the result, including the radius and calculated area
 print("The area of the circle with radius:" ,area)
 
 
-#output:-
+#Output:-
 The area of the circle with radius: 78.5
