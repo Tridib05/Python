e16ac7fd4ca78e4f843af7045ef062fc36933b68
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-09-17 11:40:59 +0530

    Create 28.avg of three no using func in python.py

---
+++ 28.avg of three no using func in python.py
@@ -0,0 +1,7 @@
+def average_of_three(num1, num2, num3):
+  return (num1 + num2 + num3) / 3
+result = average_of_three(5, 10, 15)
+print("The average of three numbers is:",result) 
+
+#Output:
+The average of three numbers is: 10.0
