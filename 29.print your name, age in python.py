3a4e9c9f13fcd65e369c64916b27a83a7b6a5b23
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-09-17 11:42:20 +0530

    Create 29.print your name, age in python.py

---
+++ 29.print your name, age in python.py
@@ -0,0 +1,8 @@
+first_name = "Tridib"
+last_name = "Bhunia"
+age = 20  
+
+print(f"My first name is {first_name} and last name is {last_name} and I am {age} years old.")
+
+#Output:
+My first name is Tridib and last name is Bhunia and I am 20 years old.
