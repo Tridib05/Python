cd5e0a7ffee54933c738a11cd02fba3b7cbd9a18
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-09-17 10:42:19 +0530

    Create 25.celcius to fahrenheit in python.py

---
+++ 25.celcius to fahrenheit in python.py
@@ -0,0 +1,6 @@
+Celcius=47
+Fahrenheit= (Celcius*1.8)+32
+print("%2f Celcius isequivalent to %2f Fahrenheit" %(Celcius,Fahrenheit))
+
+#Output:
+47.000000 Celcius isequivalent to 116.600000 Fahrenheit
