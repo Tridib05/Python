fed8df6787f95c357ce2c8947639bf72ef21e88a
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-09-17 10:38:57 +0530

    Create 24.reverse a string in python.py

---
+++ 24.reverse a string in python.py
@@ -0,0 +1,12 @@
+def reverse(string):
+    string= string[::-1]
+    return string
+str="Python"
+print("The orginal string is:", end=" ")
+print(str)
+print("The reversed string is:",end=" ")
+print(reverse(str))
+
+#Output:
+The orginal string is: Python
+The reversed string is: nohtyP
