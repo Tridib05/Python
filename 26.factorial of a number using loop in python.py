46e7ec3eb3cacb5cb7a3dcd35de5212b301eeaaa
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-09-17 10:45:44 +0530

    Create 26.factorial of a number using loop in python.py

---
+++ 26.factorial of a number using loop in python.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a number to find ouit it's factorial:"))
+fact = 1
+for i in range(1, n + 1):
+    fact *= i
+print("The factorial of", n, "is=",fact )
+
+#output:
+Enter a number to find ouit it's factorial: 5
+The factorial of 6 is= 720
