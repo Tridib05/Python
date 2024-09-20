a7df6f0befc02b43a0ebfaf9a4b196c1a9494939
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-09-20 23:17:46 +0530

    Create 36.lambda function in python.py

---
+++ 36.lambda function in python.py
@@ -0,0 +1,27 @@
+#sqaure of a number
+X=lambda a:a*a
+print(X(15))
+print(X(25))
+print(X(35))
+print(X(45))
+
+#Output:
+25
+625
+1225
+2025
+
+
+
+#add two numbers
+Y=lambda a,b:a+b
+print(Y(5,7))
+print(Y(6,8))
+print(Y(4,9))
+print(Y(7,3))
+
+#Output:
+12
+14
+13
+10
