a8053dddc89d23af9773f6e4ee25231d498eb969
Tridib Bhunia <tridibbhunia17@gmail.com>
2024-09-20 23:47:15 +0530

    Create 37.sqaure elements in a list in python.py

---
+++ 37.sqaure elements in a list in python.py
@@ -0,0 +1,32 @@
+#using for loop
+li=[1,2,3,4,5]
+li=[i**2 for i in li]
+print(li)
+
+#Output:
+[1, 4, 9, 16, 25]
+
+
+
+#using append
+li1=[1,2,3,4,5]
+li2=[]
+for num in li1:
+    x=num*num
+    li2.append(x)
+print(li2)
+
+#Output:
+[1, 4, 9, 16, 25]
+
+
+
+
+#using enumerate
+li=[1,2,3,4,5]
+for i,v in enumerate(li):
+    li[i]=v**2
+print(li)    
+
+#Output:
+[1, 4, 9, 16, 25]
